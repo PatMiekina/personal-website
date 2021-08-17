56bd2a729551eed4ff8aa0877988475a7ce415e2
Patrycja Miekina <patmiekina@gmail.com>
2021-08-17 14:46:26 +0100

    layout changes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
-  get '/work', to 'pages#work'
-  get '/about', to 'pages#about'
-  get '/experience', to 'pages#experience'
-  get '/contact', to 'pages#contact'
+  get '/work', to: 'pages#work'
+  get '/about', to: 'pages#about'
+  get '/experience', to: 'pages#experience'
+  get '/contact', to: 'pages#contact'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
